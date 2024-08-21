eb39b799bb447d66d9c1ac154c66f73a4bea57b7
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-08-21 10:55:21 +0300

    feat(classNames): add filter for additional classes

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,7 +1,11 @@
-export const classNames = (base: string, conditional?: Record<string, boolean>, additional?: string[]): string => {
+export const classNames = (
+    base: string,
+    conditional: Record<string, boolean> = {},
+    additional: string[] = []
+): string => {
     return [
         base,
         ...Object.keys(conditional).filter((k) => conditional[k]),
-        ...additional,
+        ...additional.filter(Boolean),
     ].join(' ');
 };
