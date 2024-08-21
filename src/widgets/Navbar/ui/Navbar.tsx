4db0db13ccbe88c1fc10edfccee3055ee2ee25f6
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-08-21 10:57:54 +0300

    feat: add Navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+type NavbarProps = {
+  className?: string;
+  handlerDrawer?: () => void;
+};
+
+export const Navbar = ({ className, handlerDrawer }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <FontAwesomeIcon
+        icon={faBars}
+        onClick={handlerDrawer}
+        className={classNames(cls.btnMenu)}
+      />
+    </div>
+  );
+};
