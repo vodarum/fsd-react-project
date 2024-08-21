1e2275a8d083c858799b3e7324e61237e360e433
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-08-21 10:56:17 +0300

    feat: add ThemeSwitcher

---
+++ src/features/switch-theme/ui/ThemeSwitcher.tsx
@@ -0,0 +1,28 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./ThemeSwitcher.module.scss";
+import { Button, ButtonVariants } from "shared/ui/Button";
+import { Themes, useTheme } from "shared/lib/theme";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+
+type ThemeSwitcherProps = {
+  className?: string;
+};
+
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      onClick={toggleTheme}
+      variant={ButtonVariants.clear}
+      className={classNames(cls.themeSwitcher, {}, [className])}
+    >
+      {theme === Themes.light ? (
+        <FontAwesomeIcon icon={faMoon} />
+      ) : (
+        <FontAwesomeIcon icon={faSun} />
+      )}
+    </Button>
+  );
+};
