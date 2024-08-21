6a6dbaf0e49409a3aff021c8da306c3ed6724ce3
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-08-21 10:56:31 +0300

    feat: add LangSwitcher

---
+++ src/features/switch-lang/ui/LangSwitcher.tsx
@@ -0,0 +1,26 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./LangSwitcher.module.scss";
+import { Button, ButtonVariants } from "shared/ui/Button";
+import { useTranslation } from "react-i18next";
+
+type LangSwitcherProps = {
+  className?: string;
+  short?: boolean;
+};
+
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
+  const { t, i18n } = useTranslation();
+  const handleClick = () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  };
+
+  return (
+    <Button
+      onClick={handleClick}
+      variant={ButtonVariants.clear}
+      className={classNames(cls.langSwitcher, {}, [className])}
+    >
+      {short ? t("Короткий язык") : t("Язык")}
+    </Button>
+  );
+};
