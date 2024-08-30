f1aa5b1438ae05b310cd8be3176a887a26bc12a8
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-08-30 09:52:20 +0300

    feat(Navbar): add login/logout buttons

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,13 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonVariants } from 'shared/ui/Button';
+import { useCallback, useState } from 'react';
+import { LoginModal } from 'features/login';
+import { LogoutButton } from 'features/logout';
+import { selectIsAuth } from 'entities/user/model/selectors';
+import { useSelector } from 'react-redux';
 
 type NavbarProps = {
     className?: string;
@@ -9,6 +16,19 @@ type NavbarProps = {
 };
 
 export const Navbar = ({ className, handlerDrawer }: NavbarProps) => {
+    const { t } = useTranslation('navigation');
+    const isAuth = useSelector(selectIsAuth);
+
+    const [isLoginModal, setIsLoginModal] = useState(false);
+
+    const handleLoginModalOpen = useCallback(() => {
+        setIsLoginModal(true);
+    }, []);
+
+    const handleLoginModalClose = useCallback(() => {
+        setIsLoginModal(false);
+    }, []);
+
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
             <FontAwesomeIcon
@@ -16,6 +36,24 @@ export const Navbar = ({ className, handlerDrawer }: NavbarProps) => {
                 onClick={handlerDrawer}
                 className={classNames(cls.btnMenu)}
             />
+
+            {isAuth ? (
+                <LogoutButton />
+            ) : (
+                <>
+                    <Button
+                        onClick={handleLoginModalOpen}
+                        variant={ButtonVariants.outlined}
+                    >
+                        {t('Авторизоваться')}
+                    </Button>
+
+                    <LoginModal
+                        isOpen={isLoginModal}
+                        onClose={handleLoginModalClose}
+                    />
+                </>
+            )}
         </div>
     );
 };
