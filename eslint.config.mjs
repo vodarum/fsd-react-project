508f8913533e81fbb6e5a8b3aba41e0d6703bcef
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-08-30 16:53:41 +0300

    build(eslint): add rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,15 +16,12 @@ export default [
             i18next: pluginI18next,
         },
         rules: {
-            indent: [
-                'error',
-                4,
-                { SwitchCase: 1 },
-            ],
+            indent: ['error', 4, { SwitchCase: 1 }],
             'i18next/no-literal-string': [
                 'error',
                 { markupOnly: true, ignoreAttribute: ['data-testid'] },
             ],
+            'react/display-name': 'off',
         },
         settings: {
             react: {
