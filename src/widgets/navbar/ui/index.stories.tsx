cef1edca84a80bd61c22f912fe7e59c19d777266
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-09-23 20:29:34 +0300

    test(widgets/navbar): update story

--- src/widgets/navbar/ui/index.stories.tsx
+++ src/widgets/navbar/ui/index.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Navbar } from '.';
 import { StoreDecorator } from 'shared/config/storybook/store-decorator';
+import { MenuButton } from 'shared/ui/menu-button';
+import { SettingsButton } from 'shared/ui/settings-button';
+import { mockSession } from 'entities/user';
 
 const meta = {
     title: 'widgets/Navbar',
@@ -30,17 +33,26 @@ export const Default: Story = {
     decorators: [StoreDecorator({})],
 };
 
+export const WithPassedPropsButtons: Story = {
+    decorators: [StoreDecorator({})],
+    args: {
+        append: <SettingsButton />,
+        prepend: <MenuButton />,
+    },
+};
+
 export const ForLoggedUser: Story = {
     decorators: [
         StoreDecorator({
             preloadedState: {
                 user: {
-                    session: {
-                        userId: 1,
-                        token: 'qwerty123',
-                    },
+                    session: mockSession,
                 },
             },
         }),
     ],
+    args: {
+        append: <SettingsButton />,
+        prepend: <MenuButton />,
+    },
 };
