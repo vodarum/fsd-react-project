aed0e5e3da347832435b1a62042e4906879803ed
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-02 18:36:28 +0300

    build(jest): add mock for files

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from "path";
+
 module.exports = {
     // All imported modules in your tests should be mocked automatically
     automock: false,
@@ -90,6 +92,7 @@ module.exports = {
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     moduleNameMapper: {
         // "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(__dirname, 'jestComponentMock.tsx'),
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
 
---
+++ config/jest/jestComponentMock.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+
+const jestComponentMock = () => <div />;
+
+export default jestComponentMock;
