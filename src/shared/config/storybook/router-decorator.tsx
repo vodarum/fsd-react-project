1595661495afdb71057b7841250dd55b2da02190
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-03 13:09:46 +0300

    feat(RouteDecorator): add context

--- src/shared/config/storybook/router-decorator.tsx
+++ src/shared/config/storybook/router-decorator.tsx
@@ -1,8 +1,27 @@
-import { StoryFn } from '@storybook/react';
-import { BrowserRouter } from 'react-router-dom';
+import { StoryContext, StoryFn } from '@storybook/react';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
 
-export const RouterDecorator = (Story: StoryFn) => (
-    <BrowserRouter>
-        <Story />
-    </BrowserRouter>
-);
+export const RouterDecorator = (
+    Story: StoryFn,
+    { parameters }: StoryContext,
+) => {
+    const { router } = parameters;
+
+    if (!router) {
+        return (
+            <BrowserRouter>
+                <Story />
+            </BrowserRouter>
+        );
+    }
+
+    const { path, route } = router;
+
+    return (
+        <MemoryRouter initialEntries={[encodeURI(route)]}>
+            <Routes>
+                <Route path={path} element={<Story />} />
+            </Routes>
+        </MemoryRouter>
+    );
+};
