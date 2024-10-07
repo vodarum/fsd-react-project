f165bef6f8425270ae9f2745b6af7a6ef27db0f5
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-07 12:08:32 +0300

    feat(pages/articles): update component

--- src/pages/articles/ui/index.stories.tsx
+++ src/pages/articles/ui/index.stories.tsx
@@ -1,5 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Articles from '.';
+import { StoreDecorator } from 'shared/config/storybook/store-decorator';
+import {
+    articleListReducer,
+    mockArticleListEntityState,
+    mockArticleListState,
+} from 'entities/article';
 
 const meta = {
     title: 'pages/Articles',
@@ -13,6 +19,18 @@ const meta = {
                 </main>
             </div>
         ),
+        StoreDecorator({
+            preloadedReducer: {
+                articleList: articleListReducer,
+            },
+            preloadedState: {
+                articleList: {
+                    ...mockArticleListState,
+                    entities: mockArticleListEntityState.entities,
+                    ids: mockArticleListEntityState.ids,
+                },
+            },
+        }),
     ],
 } satisfies Meta<typeof Articles>;
 
--- src/pages/articles/ui/index.tsx
+++ src/pages/articles/ui/index.tsx
@@ -1,8 +1,61 @@
+import {
+    ArticleList,
+    articleListActions,
+    articleListReducer,
+    articleListSelectors,
+    ArticleViewType,
+    fetchList,
+} from 'entities/article';
+import { ArticleViewSwitcher } from 'features/article';
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import {
+    useAppDispatch,
+    useAsyncStore,
+    useInitialEffect,
+} from 'shared/lib/hooks';
+import cls from './index.module.scss';
 
 const Articles = () => {
-    const { t } = useTranslation('articles');
-    return <h1>{t('Статьи')}</h1>;
+    const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const articles = useSelector(articleListSelectors.selectAll);
+    const loading = useSelector(articleListSelectors.selectLoading);
+    const error = useSelector(articleListSelectors.selectError);
+    const view = useSelector(articleListSelectors.selectView);
+
+    const handleArticleViewBtnClick = useCallback(
+        (view: ArticleViewType) => dispatch(articleListActions.setView(view)),
+        [dispatch],
+    );
+
+    useAsyncStore({
+        articleList: articleListReducer,
+    });
+
+    useInitialEffect(() => {
+        // @ts-ignore
+        dispatch(fetchList());
+    }, []);
+
+    return (
+        <>
+            <h1 className={cls.title}>{t('Статьи')}</h1>
+
+            <ArticleViewSwitcher
+                onViewClick={handleArticleViewBtnClick}
+                view={view}
+            />
+
+            <ArticleList
+                items={articles}
+                error={error}
+                loading={loading}
+                view={view}
+            />
+        </>
+    );
 };
 
-export default Articles;
+export default memo(Articles);
