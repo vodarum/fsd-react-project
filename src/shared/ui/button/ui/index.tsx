3cf8bad4ee6be2681811d4390f1555945a8cbfab
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-17 18:11:24 +0300

    feat(Button): add forwardRef

--- src/shared/ui/button/ui/index.tsx
+++ src/shared/ui/button/ui/index.tsx
@@ -1,6 +1,12 @@
 import { classNames } from 'shared/lib/class-names';
 import cls from './index.module.scss';
-import { ButtonHTMLAttributes, FC, memo, PropsWithChildren } from 'react';
+import {
+    ButtonHTMLAttributes,
+    ForwardedRef,
+    forwardRef,
+    memo,
+    PropsWithChildren,
+} from 'react';
 
 const ButtonVariants = {
     clear: 'clear',
@@ -15,22 +21,28 @@ type ButtonProps = PropsWithChildren &
         variant?: ButtonVariant;
     };
 
-const Button: FC<ButtonProps> = memo((props: ButtonProps) => {
-    const {
-        children,
-        className,
-        variant = ButtonVariants.outlined,
-        ...otherProps
-    } = props;
+const Button = memo(
+    forwardRef((props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+        const {
+            children,
+            className,
+            variant = ButtonVariants.outlined,
+            ...otherProps
+        } = props;
 
-    return (
-        <button
-            className={classNames(cls.button, {}, [className, cls[variant]])}
-            {...otherProps}
-        >
-            {children}
-        </button>
-    );
-});
+        return (
+            <button
+                ref={ref}
+                className={classNames(cls.button, {}, [
+                    className,
+                    cls[variant],
+                ])}
+                {...otherProps}
+            >
+                {children}
+            </button>
+        );
+    }),
+);
 
 export { Button, ButtonVariants };
