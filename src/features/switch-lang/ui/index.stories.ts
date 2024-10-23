2515e0ddd53ae788e83858aadf44a742982fca94
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-23 18:10:01 +0300

    feat(LangSwitcher): change Button to Dropdown

--- src/features/switch-lang/ui/index.stories.ts
+++ src/features/switch-lang/ui/index.stories.ts
@@ -14,8 +14,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-export const Short: Story = {
-    args: {
-        short: true,
-    },
-};
--- src/features/switch-lang/ui/index.tsx
+++ src/features/switch-lang/ui/index.tsx
@@ -1,27 +1,45 @@
 import { classNames } from 'shared/lib/class-names';
 import cls from './index.module.scss';
-import { Button, ButtonVariants } from 'shared/ui//button';
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
+import { memo, useCallback, useMemo } from 'react';
+import { Dropdown, type DropdownItem } from 'shared/ui/popups';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+
+const langs = {
+    ru: 'Русский',
+    en: 'Английский',
+} as const;
 
 type LangSwitcherProps = {
     className?: string;
-    short?: boolean;
 };
 
-export const LangSwitcher = memo(({ className, short }: LangSwitcherProps) => {
+export const LangSwitcher = memo(({ className }: LangSwitcherProps) => {
     const { t, i18n } = useTranslation();
-    const handleClick = () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-    };
+    const handleClick = useCallback(
+        (lang: string) => () => {
+            i18n.changeLanguage(lang);
+        },
+        [i18n],
+    );
+
+    const items = useMemo(
+        () =>
+            Object.entries(langs).map<DropdownItem>(([k, v]) => ({
+                content: t(v),
+                onClick: handleClick(k),
+                active: i18n.language === k,
+            })),
+        [i18n, t, handleClick],
+    );
 
     return (
-        <Button
-            onClick={handleClick}
-            variant={ButtonVariants.clear}
+        <Dropdown
             className={classNames(cls.langSwitcher, {}, [className])}
-        >
-            {short ? t('Короткий язык') : t('Язык')}
-        </Button>
+            anchor='bottom end'
+            activator={<FontAwesomeIcon icon={faLanguage} />}
+            items={items}
+        />
     );
 });
