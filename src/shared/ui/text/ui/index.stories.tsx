2d00fb688b5f1cc2667ca6ea7b02a9af3d82dc4f
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-25 23:59:09 +0300

    feat(Text): add align prop

--- src/shared/ui/text/ui/index.stories.tsx
+++ src/shared/ui/text/ui/index.stories.tsx
@@ -5,11 +5,12 @@ const meta = {
     title: 'shared/Text',
     component: Text,
     parameters: {
-        layout: 'centered',
+        layout: 'padded',
     },
     tags: ['autodocs'],
     args: {
-        children: 'Text',
+        children:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
 } satisfies Meta<typeof Text>;
 
@@ -17,3 +18,27 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const AlignCenter: Story = {
+    args: {
+        align: 'center',
+    },
+};
+
+export const AlignJustify: Story = {
+    args: {
+        align: 'justify',
+    },
+};
+
+export const AlignLeft: Story = {
+    args: {
+        align: 'left',
+    },
+};
+
+export const AlignRight: Story = {
+    args: {
+        align: 'right',
+    },
+};
--- src/shared/ui/text/ui/index.tsx
+++ src/shared/ui/text/ui/index.tsx
@@ -1,13 +1,18 @@
+import { PropsWithClassName } from '@/shared/api';
 import { memo, ReactNode } from 'react';
 
-type TextProps = {
-    className?: string;
+type TextProps = PropsWithClassName & {
     children?: ReactNode;
+    align?: 'left' | 'right' | 'center' | 'justify';
 };
 
 const Text = memo((props: TextProps) => {
-    const { className, children } = props;
-    return <p className={className}>{children}</p>;
+    const { className, children, align } = props;
+    return (
+        <p className={className} style={{ textAlign: align }}>
+            {children}
+        </p>
+    );
 });
 
 export { Text };
