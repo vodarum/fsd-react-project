4d5b9d9a5099b1a47a9e15a260bd3664beebf584
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-10-29 13:17:53 +0300

    build: add webp image to file loader config

--- config/build/loaders/build-file-loader.ts
+++ config/build/loaders/build-file-loader.ts
@@ -1,6 +1,6 @@
 import { RuleSetRule } from 'webpack';
 
 export const buildFileLoader = (): RuleSetRule => ({
-    test: /\.(png|jpe?g|gif)$/i,
+    test: /\.(png|jpe?g|webp|gif)$/i,
     loader: 'file-loader',
 });
--- src/app/types/globals.d.ts
+++ src/app/types/globals.d.ts
@@ -3,6 +3,7 @@ declare module '*.module.scss';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
+declare module '*.webp';
 declare const __IS_DEV__: boolean;
 declare const __BASE_URL__: string;
 declare const __PROJECT__: 'frontend' | 'storybook' | 'jest';
