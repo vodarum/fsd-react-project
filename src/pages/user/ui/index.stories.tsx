7e697b9a6def7fb2825caebf708d8e8cfb35c7a4
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-11-07 13:26:31 +0300

    test(pages/user): update stories

--- src/pages/user/ui/index.stories.tsx
+++ src/pages/user/ui/index.stories.tsx
@@ -1,34 +1,55 @@
+import { delay, http, HttpResponse } from 'msw';
 import type { Meta, StoryObj } from '@storybook/react';
 import User from '.';
 import { mockUsers } from '@/entities/user/testing';
-import { mockUserState, userReducer } from '@/features/user/testing';
 import { PageDecorator } from '@/shared/config/storybook/page-decorator';
 import { StoreDecorator } from '@/shared/config/storybook/store-decorator';
+
+const mockUser = mockUsers[0];
+const route = `/users/${mockUser.id}`;
+const url = `${__BASE_URL__}${route}`;
 
 const meta = {
     title: 'pages/User',
     component: User,
     tags: ['autodocs'],
     decorators: [PageDecorator, StoreDecorator({})],
+    parameters: {
+        router: {
+            path: '/users/:id',
+            route,
+        },
+    },
 } satisfies Meta<typeof User>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    decorators: [
-        StoreDecorator({
-            preloadedReducer: {
-                user: userReducer,
-            },
-            preloadedState: {
-                user: {
-                    ...mockUserState,
-                    data: mockUsers[0],
-                },
-            },
-        }),
-    ],
+    parameters: {
+        msw: {
+            handlers: [http.get(url, () => HttpResponse.json(mockUser))],
+        },
+    },
 };
 
-export const NotFound: Story = {};
+export const Loading: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+                http.get(url, async () => {
+                    await delay('infinite');
+                    return HttpResponse.json(mockUser);
+                }),
+            ],
+        },
+    },
+};
+
+export const NotFound: Story = {
+    parameters: {
+        msw: {
+            handlers: [http.get(url, () => HttpResponse.error())],
+        },
+    },
+};
