2574ea9799a10284f63143200d3bb4b958aeb3bf
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2024-11-14 22:22:29 +0300

    build(ESLint): add rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import pluginI18next from 'eslint-plugin-i18next';
 import pluginUnusedImports from 'eslint-plugin-unused-imports';
+import pluginFsd from './plugins/eslint-plugin-fsd/index.js';
 
 export default [
     { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -16,9 +17,16 @@ export default [
         plugins: {
             i18next: pluginI18next,
             'unused-imports': pluginUnusedImports,
+            fsd: pluginFsd,
         },
         rules: {
-            indent: ['error', 4, { SwitchCase: 1 }],
+            indent: [
+                'error',
+                4,
+                {
+                    SwitchCase: 1,
+                },
+            ],
             'i18next/no-literal-string': [
                 'error',
                 { markupOnly: true, ignoreAttribute: ['data-testid'] },
@@ -26,6 +34,50 @@ export default [
             'react/display-name': 'off',
             '@typescript-eslint/ban-ts-comment': 'off',
             'unused-imports/no-unused-imports': 'error',
+            'fsd/path-checker': ['error', { alias: '@' }],
+            'fsd/public-api-imports': [
+                'error',
+                {
+                    alias: '@',
+                    ignoreImportPatterns: [
+                        '**/(error-boundary|(store|theme)-provider)',
+                        '**/app/styles/index.scss',
+                    ],
+                },
+            ],
+            'fsd/testing-public-api-imports': [
+                'error',
+                {
+                    alias: '@',
+                    testFilePatterns: [
+                        '**/*.test.ts',
+                        '**/*.(test|stories).tsx',
+                        '**/__mocks__/*.ts',
+                    ],
+                },
+            ],
+            'fsd/mock-imports': [
+                'error',
+                {
+                    alias: '@',
+                    testFilePatterns: [
+                        '**/*.test.ts',
+                        '**/*.(test|stories).tsx',
+                    ],
+                },
+            ],
+            'fsd/mock-exports': ['error', { alias: '@' }],
+            'fsd/layer-imports': [
+                'error',
+                {
+                    alias: '@',
+                    ignoreImportPatterns: [
+                        '**/store-provider',
+                        '**/app/styles/index.scss',
+                        '**/testing',
+                    ],
+                },
+            ],
         },
         settings: {
             react: {
